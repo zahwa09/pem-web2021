8c4dabe423c32d88de0d3218ed09ded5f0c3c3be
zahwa09 <92170243+zahwa09@users.noreply.github.com>
2022-01-20 22:27:43 +0700

    Add files via upload

---
+++ TA PBW/script.js
@@ -0,0 +1,10 @@
+const login = document.getElementById("login");
+const registrasi = document.getElementById("registasi");
+const home = document.getElementById("home");
+const tell = document.getElementById("tell");
+const motivation = document.getElementById("motivation");
+
+home.addEventListener(tell, function () {
+  tell.classList.add("relieved");
+  motivation.classList.add(returnn);
+});
